d1bd82f03138df57be47a39cef77db6e96c06545
Thanh Cong Nguyen Tong <shin141106@gmail.com>
2025-11-10 10:38:48 +0700

    Time: 1770 ms (20.92%) | Memory: 18.2 MB (95.95%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,12 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_map:
-                # If the complement exists in our map, we've found the solution.
-                return [num_map[complement], i]
-            # Otherwise, add the current number and its index to the map.
-            num_map[num] = i
-        #heheheh2
-        
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
